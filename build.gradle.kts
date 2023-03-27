eefbcd442c547ff5bd08a8498bb1987bec5db3fd
Florian <37657783+flou12321@users.noreply.github.com>
2023-03-28 01:25:48 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ repositories {
 }
 
 dependencies {
-    implementation("net.dv8tion:JDA:4.3.0_277")
+    implementation("com.github.DV8FromTheWorld:JDA:legacy~v4-SNAPSHOT")
     //implementation("com.sedmelluq:lavaplayer:1.3.78")
     implementation("com.github.walkyst:lavaplayer-fork:1.3.96")
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
